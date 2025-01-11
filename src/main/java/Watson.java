8d10a2447afda734df7e9f80317bd9ed51374c20
SherrinfordHR <sherryhrobin@gmail.com>
2025-01-11 16:44:03 +0800

    level 1-1 Use scanner to read input and while loop till "bye" is read

--- src/main/java/Watson.java
+++ src/main/java/Watson.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
+
 public class Watson {
     public static void main(String[] args) {
         String botname = "Watson";
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hello! I'm " + botname);
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        while(true) {
+            String command = scanner.nextLine();
+            if (command.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(command);
+            }
+        }
     }
 }
