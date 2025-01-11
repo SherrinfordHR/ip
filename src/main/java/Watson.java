9cc0c0d8f75a99f01d406f2fe8234dc9a53007f4
SherrinfordHR <sherryhrobin@gmail.com>
2025-01-11 16:08:39 +0800

    level 1-0 Rename to Watson, Greet, Exit

---
+++ src/main/java/Watson.java
@@ -0,0 +1,8 @@
+public class Watson {
+    public static void main(String[] args) {
+        String botname = "Watson";
+        System.out.println("Hello! I'm " + botname);
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
