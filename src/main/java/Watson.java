97f67d9feb5ae29cd0dbf4e4b0b8a8d26541aae6
SherrinfordHR <sherryhrobin@gmail.com>
2025-01-11 17:04:47 +0800

    level 1-2 use ArrayList to store the tasks and use for loop to display them one by one

--- src/main/java/Watson.java
+++ src/main/java/Watson.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Watson {
     public static void main(String[] args) {
         String botname = "Watson";
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> Stringlist = new ArrayList<String>(100);
+
         System.out.println("Hello! I'm " + botname);
         System.out.println("What can I do for you?");
         while(true) {
@@ -11,8 +14,15 @@ public class Watson {
             if (command.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
-            } else {
-                System.out.println(command);
+            }
+            else if (command.equals("list")){
+                for(int i = 0; i < Stringlist.size(); i++){
+                    System.out.println(i+1 + ". " + Stringlist.get(i));
+                }
+            }
+            else {
+                Stringlist.add(command);
+                System.out.println("added: " + command);
             }
         }
     }
